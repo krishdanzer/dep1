0899a4cd8801f023137348a2c76bf923302125a6
krishdanzer <101338757+krishdanzer@users.noreply.github.com>
2022-03-10 22:50:05 +0530

    Update RollDice.js

--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -60,10 +60,7 @@ const RollDice = ({ sides }) => {
 RollDice.defaultProps = {
     sides: [
         {one: 1},
-        {two: 2},
-        {three: 3},
-        {four: 4},
-        {five: 5},
+    
         {six: 6},
     ],
 };
